b2884b33b9eca76892e642a7eeafff9ea6579fde
AIT EL FAQIR Marouane <marouane.ait-el-faqir@csgroup.eu>
2023-05-13 17:30:59 +0200

    encoder implementation

--- model.py
+++ model.py
@@ -137,51 +137,70 @@ class Encoder(nn.Module):
         self.z_dim = z_dim
 
         self.encStack = nn.Sequential(
-            nn.Conv2d(3, 32, 5, stride=1, bias=False),
+            nn.Conv2d(1, 32, 3, stride=1, bias=False),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(32, 64, 4, stride=2, bias=False),
+            nn.Conv2d(32, 64, 3, stride=1, bias=False),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(64, 128, 4, stride=1, bias=False),
+            nn.Conv2d(64, 128, 3, stride=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(128, 256, 4, stride=2, bias=False),
+            nn.Conv2d(128, 256, 3, stride=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(256, 512, 4, stride=1, bias=False),
+            nn.Conv2d(256, 512, 3, stride=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(512, 512, 1, stride=1, bias=False),
-            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 2*512, 1, stride=1, bias=False),
+            nn.BatchNorm2d(2*512),
             nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(512, z_dim*2, 1, stride=1, bias=True),
-            nn.Sigmoid()
+          
+            # nn.Conv2d(512, 2*z_dim, 1, stride=1, bias=True),
+           
+            # nn.Sigmoid()
+            
 
         )
+        self.mu_layer = nn.Linear(1024*2*2, z_dim)
+        self.logvar_layer = nn.Linear(1024*2*2, z_dim)
 
-    def reparametrize(self, z):
-        z = z.view(z.size(0), -1)
-        mu, log_var = z[:, :self.z_dim], z[:,self.z_dim:]
-        std = torch.exp(0.5* log_var)
-        epsilon = torch.randn_like(std)
+    # def reparametrize(self, z):
+    #     # z = z.view(z.size(0), -1)
+    #     # print(f"z size: {z.size()}")
+    #     mu, log_var = z[:, :self.z_dim], z[:,self.z_dim:]
+    #     std = torch.exp(0.5* log_var)
+    #     epsilon = torch.randn_like(std)
         
+    #     print(f"mu size : {mu.size()}")
+    #     print(f"std size: {std.size()}")
 
-        sample = mu + epsilon*std
-        return sample 
+    #     sample = mu + epsilon*std
+    #     return sample 
+    def reparametrize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
 
     def forward(self, x):
-
         x = self.encStack(x)
-        z_sample = self.reparametrize(x)
+        x = x.view(-1, 1024*2*2)
+        mu = self.mu_layer(x)
+        logvar = self.logvar_layer(x)
+        z_sample = self.reparametrize(mu, logvar)
+        return z_sample, mu, logvar
+    
+    # def forward(self, x):
 
-        return z_sample.view(x.size(0), self.z_dim, 1, 1)
+    #     x = self.encStack(x)
+    #     z_sample = self.reparametrize(x)
+
+    #     return z_sample.view(x.size(0), self.z_dim, 1, 1)
 
 
 

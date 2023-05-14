1b853ed77cb2a3e6bd21e36f72965a00752b1ea7
AIT EL FAQIR Marouane <marouane.ait-el-faqir@csgroup.eu>
2023-05-14 07:58:01 +0200

    add: discriminator and generator tested

--- model.py
+++ model.py
@@ -99,6 +99,7 @@ class Generator(nn.Module):
 
         self.z_dim = z_dim
         self.output_bias = nn.Parameter(torch.zeros(1, 28, 28), requires_grad=True)
+        
         self.dec_stack = nn.Sequential(
             nn.ConvTranspose2d(z_dim, 256, 5, stride=1, bias=False),
             nn.BatchNorm2d(256),
@@ -120,11 +121,11 @@ class Generator(nn.Module):
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1, inplace=True),
 
+            
+            nn.ConvTranspose2d(32, 1, 1, stride=1, bias=True),
             nn.LeakyReLU(0.1, inplace=True),
-
-
-            nn.Conv2d(32, 1, 1, stride=1, bias=True),
-
+            
+            
             nn.Sigmoid()
             )
         
@@ -188,102 +189,3 @@ class Generator(nn.Module):
         x_z = self.decoding(z)
 
         return x_z, z
-
-
-        
-class Decoder(nn.Module):
-    def __init__(self, z_dim=32):
-        super().__init__()
-        self.z_dim = z_dim
-        self.output_bias = nn.Parameter(torch.zeros(1, 28, 28), requires_grad=True)
-
-        self.genStack = nn.Sequential(
-            nn.ConvTranspose2d(z_dim, 256, 5, stride=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.ConvTranspose2d(256, 128, 5, stride=2, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.ConvTranspose2d(128, 64, 5, stride=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.ConvTranspose2d(64, 32, 6, stride=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.ConvTranspose2d(32, 32, 7, stride=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.LeakyReLU(0.1, inplace=True),
-
-
-            nn.ConvTranspose2d(32, 1, 1, stride=1, bias=True),
-
-            nn.Sigmoid()
-            )
-
-    def forward(self, z):
-
-        z = self.genStack(z)
-        print(f"decoder shape: {z.shape}")
-        return torch.sigmoid(z + self.output_bias)
-
-
-class Encoder(nn.Module):
-    def __init__(self, z_dim=32):
-
-        super().__init__()
-        self.z_dim = z_dim
-
-        self.encStack = nn.Sequential(
-            nn.Conv2d(in_channels=1, 
-                      out_channels=32, 
-                      kernel_size=4, 
-                      stride=1, 
-                      padding=1, 
-                      bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1, inplace=True),
-
-            nn.Conv2d(512, 512, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1, inplace=True),
-          
-            nn.Conv2d(512, 2*z_dim, 1, stride=1, bias=True),
-           
-            # nn.Sigmoid()
-        )
-        
-    def reparametrize(self, z):
-        mu, log_var = z[:, :self.z_dim], z[: ,self.z_dim:]
-        std = torch.exp(.5 * log_var)
-        epsilon = torch.randn_like(std)
-        sample = mu + epsilon*std
-        return sample
-
-    def forward(self, x):
-        x = self.encStack(x)
-        x = x.view(x.size(0), -1)
-        z_sample = self.reparametrize(x)
-        return z_sample
-
